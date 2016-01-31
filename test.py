ac993856533e96af5aa1feddc57ade06277f9680
Leo Khachatorians <leokhachatorians@gmail.com>
2016-01-30 16:51:38 -0800

    Add playlist functionality
    Messing around with how to download tracks in a playlist. Need to also refactor some code.

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 import soundcloud
 import urllib
 import argparse
+import sys
+
+
+class Parser(argparse.ArgumentParser):
+        def error(self, error):
+                sys.stderr.write("\nError: {}\n".format(error))
+                self.print_help()
+                sys.exit(2)
+
 
 CLIENT_ID = "a1a76b5205bb80d8f7ad182133028016"
 client = soundcloud.Client(client_id=CLIENT_ID)
-parser = argparse.ArgumentParser(description="Sonic, a simple SoundCloud downloader.")
+parser = Parser(description="Sonic, a simple SoundCloud downloader.")
 parser.add_argument(
 	'-d','--download',
-	help="Downloads Given Tracks",
+	help="Downloads given tracks via URL, space delimited",
 	nargs="*")
+
+parser.add_argument(
+        '-l', '--login',
+        help="Login to access your liked tracks and other features",
+        type=str)
 
 url = "http://soundcloud.com/deepmixnation/best-vocal-deep-uk-house-music-113-by-xypo"
 
@@ -36,22 +50,40 @@ def download_tracks_workflow(urls):
 		try:
 			track_info = get_track_info_via_url(client, url)
 			stream_info = fetch_track_to_stream(client, track_info.id)
-			# print(track_info, track_info.title, track_info.id)
 			stream_url = get_stream_url(client, stream_info)
-			# print(stream_url.location)
 			track_name = format_track_name(track_info.title)
+			print('Starting downlaod of {}'.format(track_name))
 			download_track(stream_url.location, track_name)
 			print('Succesfully downloaded {}'.format(track_name))
 		except Exception as e:
 			print('Caught an exception: {}'.format(e))
 
+def get_play_list(url):
+        try:
+                a = client.get('/resolve', url=url)
+                print(a)
+                return a
+        except Exception as e:
+                print(e)
 
+
+a = get_play_list("https://soundcloud.com/eemilybowmann/sets/mine")
+for i in a.tracks:
+        print(i['id'])
+        stream_info = fetch_track_to_stream(client, i['id'])
+        stream_url = get_stream_url(client, stream_info)
+        track_name = format_track_name(i['title'])
+        print(track_name)
+        download_track(stream_url.location, track_name)
+        
+"""
 if __name__ == '__main__':
 	args = parser.parse_args()
 	if args.download:
 		download_tracks_workflow(args.download)
 	else:
-		print('You need to pass a url to download!')
+		parser.print_help()
+"""
 
 # track_info = get_track_info_via_url(client, url)
 # try:
